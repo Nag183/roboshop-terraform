18ad736e3d3521a7fd07edf32c2038a900109e3c
nagendra <nagenderababu.g@gmail.com>
2023-12-20 13:01:24 +0530

    all scripts

--- servers.tf
+++ servers.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "frontend-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -31,7 +31,7 @@ resource "aws_instance" "cart" {
 }
 
 resource "aws_route53_record" "cart" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "cart-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -48,7 +48,7 @@ resource "aws_instance" "mongodb" {
 }
 
 resource "aws_route53_record" "mongodb" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "mongodb-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -65,7 +65,7 @@ resource "aws_instance" "catalogue" {
 }
 
 resource "aws_route53_record" "catalogue" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "catalogue-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -82,7 +82,7 @@ resource "aws_instance" "redis" {
 }
 
 resource "aws_route53_record" "redis" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "redis-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -99,7 +99,7 @@ resource "aws_instance" "user" {
 }
 
 resource "aws_route53_record" "user" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "user-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -116,7 +116,7 @@ resource "aws_instance" "mysql" {
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "mysql-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -133,7 +133,7 @@ resource "aws_instance" "shipping" {
 }
 
 resource "aws_route53_record" "shipping" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "shipping-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -150,7 +150,7 @@ resource "aws_instance" "rabbitmq" {
 }
 
 resource "aws_route53_record" "rabbitmq" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "rabbitmq-dev.naginfo.us"
   type    = "A"
   ttl     = 30
@@ -167,7 +167,7 @@ resource "aws_instance" "payment" {
 }
 
 resource "aws_route53_record" "payment" {
-  zone_id = Z013923922XABW4K0OEK0
+  zone_id = "Z013923922XABW4K0OEK0"
   name    = "payment-dev.naginfo.us"
   type    = "A"
   ttl     = 30
