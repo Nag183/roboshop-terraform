c8d6ea7d12d81c155cc52daa411d74b800c75e12
nagendra <nagenderababu.g@gmail.com>
2023-12-20 12:14:40 +0530

    all scripts

--- servers.tf
+++ servers.tf
@@ -11,84 +11,84 @@ output "frontend" {
   value = aws_instance.frontend.public_ip
 }
 
-#resource "aws_instance" "cart" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "cart"
-#  }
-#}
-#
-#resource "aws_instance" "mongodb" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "mongodb"
-#  }
-#}
-#
-#resource "aws_instance" "catalogue" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "catalogue"
-#  }
-#}
-#
-#resource "aws_instance" "redis" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "redis"
-#  }
-#}
-#
-#resource "aws_instance" "user" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "user"
-#  }
-#}
-#
-#resource "aws_instance" "mysql" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "mysql"
-#  }
-#}
-#
-#resource "aws_instance" "shipping" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "shipping"
-#  }
-#}
-#
-#resource "aws_instance" "rabbitmq" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "rabbitmq"
-#  }
-#}
-#
-#resource "aws_instance" "payment" {
-#  ami           = "ami-0bb6af715826253bf"
-#  instance_type = "t3.micro"
-#
-#  tags = {
-#    Name = "payment"
-#  }
-#}
+resource "aws_instance" "cart" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-0bb6af715826253bf"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "payment"
+  }
+}
 
