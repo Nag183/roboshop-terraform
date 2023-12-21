d5689f916d401ea1e250dbfdd78850468a9e6336
nagendra <nagenderababu.g@gmail.com>
2023-12-21 13:04:47 +0530

    all scripts

--- servers.tf
+++ servers.tf
@@ -11,52 +11,52 @@ data "aws_security_group" "allow-all" {
 variable "components" {
   default = {
     frontend = {
-      name = "frontend"
+      name          = "frontend"
       instance_type = "t3.small"
     }
     mongodb = {
-      name = "mongodb"
+      name          = "mongodb"
       instance_type = "t3.small"
     }
     catalogue = {
-      name = "catalogue"
+      name          = "catalogue"
       instance_type = "t3.micro"
     }
     redis = {
-      name = "redis"
+      name          = "redis"
       instance_type = "t3.small"
     }
     mysql = {
-      name = "mysql"
+      name          = "mysql"
       instance_type = "t3.small"
     }
     shipping = {
-      name = "shipping"
+      name          = "shipping"
       instance_type = "t3.medium"
     }
     payment = {
-      name = "payment"
+      name          = "payment"
       instance_type = "t3.small"
     }
     user = {
-      name = "user"
+      name          = "user"
       instance_type = "t3.micro"
     }
     cart = {
-      name = "cart"
+      name          = "cart"
       instance_type = "t3.micro"
     }
     rabbitmq = {
-      name = "rabbitmq"
+      name          = "rabbitmq"
       instance_type = "t3.small"
     }
   }
 }
 
 resource "aws_instance" "instance" {
-  for_each = var.components
-  ami           = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
@@ -64,11 +64,12 @@ resource "aws_instance" "instance" {
   }
 }
 
-#resource "aws_route53_record" "frontend" {
-#  zone_id = "Z013923922XABW4K0OEK0"
-#  name    = "frontend.dev.naginfo.us"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.frontend.private_ip]
-#}
-#
+resource "aws_route53_record" "frontend" {
+  for_each = var.components
+  zone_id  = "Z013923922XABW4K0OEK0"
+  name     = "${each.value["name"]}.dev.naginfo.us"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
+}
+
